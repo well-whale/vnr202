9ea04972113d4bf86b2d4361c16b7db7723e9fc6
ZitnotDuck <viethoicv@gmail.com>
2025-08-22 21:39:45 +0700

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { Merriweather } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const merriweather = Merriweather({
+  variable: "--font-merriweather",
+  subsets: ["latin"],
+  weight: ["300", "400", "700", "900"],
+  display: "swap",
+  preload: true,
+});
+
+export const metadata: Metadata = {
+  title: "Chiến thắng Điện Biên Phủ",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="vi">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${merriweather.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
